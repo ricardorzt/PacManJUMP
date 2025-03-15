2965a24c1c83149eea79f85373e51887826e1521
YahirTostado <yahireduardotn6@gmail.com>
2025-03-15 08:39:23 -0600

    Create creditos.js

---
+++ src/creditos.js
@@ -0,0 +1,15 @@
+function showCredits() {
+    // Ocultar el menú y el canvas
+    document.getElementById("menu").style.display = "none";
+    canvas.style.display = "none";
+    document.getElementById("dragDropArea").style.display = "none";
+    
+    // Mostrar la sección de créditos
+    document.getElementById("creditsSection").style.display = "flex";
+}
+function closeCredits() {
+    // Ocultar la sección de créditos
+    document.getElementById("creditsSection").style.display = "none";
+    document.getElementById("menu").style.display = "flex"; // Mostrar el menú
+    const canvas = document.getElementById("gameCanvas");
+    canvas.style.display = "block"; }
